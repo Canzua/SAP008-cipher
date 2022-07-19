2e55c203527561c7a8ad4c825e67a1018b88eaea
Andrea dos Santos <contato.andreadossantos@gmail.com>
2022-07-19 13:38:55 -0300

    iniciando meu projeto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+function funcao1() {
+    const "box-cifrar"
+result 
+}
+
+let texto =" ";
+function deslocarTexto(){
+    texto="";
+}
+
+document.getElementById("btnSubmit").onclick = function funcao1() {
+
+ 
+console.log(codigoDaletra + deslocamento) % + tamDoAlfabeto;
